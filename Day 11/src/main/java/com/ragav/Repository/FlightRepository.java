819acb5453c5405bf0f6c739f5d454a33d8ca817
Harish Ragav <118203265+harish24-coder@users.noreply.github.com>
2023-10-05 13:53:52 +0530

    Add files via upload

---
+++ Day 11/src/main/java/com/ragav/Repository/FlightRepository.java
@@ -0,0 +1,11 @@
+package com.ragav.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FlightRepository extends JpaRepository<Flight,Long> {
+
+	
+
+}
---
+++ Day 11/src/main/java/com/ragav/dto/FlightDTO.java
@@ -0,0 +1,5 @@
+package com.ragav.dto;
+
+public class FlightDTO {
+
+}
